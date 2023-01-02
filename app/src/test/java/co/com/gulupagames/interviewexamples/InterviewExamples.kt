7b49bd02e390184aa8aeb9767a82729df2c9e454
Yuber Garcia <yuber.garcia@globant.com>
2023-01-02 09:50:23 -0500

    Project creation: First commit

---
+++ app/src/test/java/co/com/gulupagames/interviewexamples/InterviewExamples.kt
@@ -0,0 +1,12 @@
+package co.com.gulupagames.interviewexamples
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class InterviewExamples {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
